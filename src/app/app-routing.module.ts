c6f0e816d27cd5fd05d9d2c8011449404f9e7c66
eat-sleep-code-repeat-it <rolland@DESKTOP-MU25O6E.localdomain>
2021-02-06 18:39:30 -0500

    add route to d3js or cytoscape

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CytoScapeComponent } from 'projects/cyto-scape/src/lib/cyto-scape.component';
+import { D3jsComponent } from 'projects/d3js/src/lib/d3js.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: D3jsComponent },
+  { path: 'graph-cyto', component: CytoScapeComponent },
+  { path: 'graph-d3js', component: D3jsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
